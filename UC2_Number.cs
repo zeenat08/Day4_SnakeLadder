366efb496bd8f5821aea6f331085c023916bf362
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:51:06 +0530

    ZEENAT: added random no as a dice

---
+++ UC2_Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snakeladder
+{
+    class UC2_Number
+    {
+        public static void RandomNumber()
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 6);
+            Console.WriteLine("the Random number between 0 to 6 is " +  number);
+            Console.ReadKey();
+        }
+    }
+}

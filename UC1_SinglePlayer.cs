19330ff5665123694cc10b31f8a5c78526212a4b
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:49:29 +0530

    ZEENAT: added starting position

---
+++ UC1_SinglePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snakeladder
+{
+    class UC1_SinglePlayer
+    {
+        public static void SinglePosition()
+        {
+         Console.WriteLine("The position of player is starting from 0");
+        }
+    }
+}

17260a73f07e1cbe0c9a287d89a7bc8a99c3c276
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:56:38 +0530

    ZEENAT : Added main compilation

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snakeladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC2_Number.RandomNumber();
+            UC1_SinglePlayer.SinglePosition();
+            Console.ReadKey();
+        }
+    }
+}
